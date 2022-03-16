5be1fad211f3923b58a1cbbf282055f0fcd99c69
Audamon <joaotamagno@gmail.com>
2022-03-16 14:59:47 -0300

    AppSlice was fixed

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -15,17 +15,12 @@ export const appSlice = createSlice({
       state.roomId = action.payload.roomId;
     },
 
-    updateNewUserName: (state, action) => {
-      state.userName = action.payload.userName;
-    }
   },
 
 });
 
-export const { enterRoom, updateNewUserName } = appSlice.actions;
+export const { enterRoom} = appSlice.actions;
 
 export const selectRoomId = (state) => state.app.roomId;
-
-export const updateUserName = (state) => state.app.userName;
 
 export default appSlice.reducer;
